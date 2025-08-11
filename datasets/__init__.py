f6101c57af1d849b4f9f6fee821e945b32002a5f
haokun.zhang <kevinz@1706@163.com>
2025-08-11 15:43:21 +0800

    feat: initedmot17 data

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,6 +4,7 @@ import torchvision
 
 from .coco import build as build_coco
 from .bdd100k import build as build_bddmot20
+from .mot17det import build as build_mot17det
 
 
 def get_coco_api_from_dataset(dataset):
@@ -27,5 +28,8 @@ def build_dataset(image_set, args):
     
     if args.dataset_file == "bddmot20":
         return build_bddmot20(image_set, args)
+    
+    if args.dataset_file == "mot17":
+        return build_mot17det(image_set, args)
     
     raise ValueError(f'dataset {args.dataset_file} not supported')
